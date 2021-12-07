12aba6356cda35a5c6e0e0e709dc01cf367e43f4
Sabieh Ahmed <sabieh.ahmed@10pearls.com>
2021-12-07 11:06:38 +0500

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet laboriosam omnis recusandae, soluta suscipit tempora. Beatae esse explicabo iure nulla officiis, repellat. Doloremque illum possimus, ratione soluta sunt tempore voluptatem.
                                 </div>
                             </div>
                         </div>
